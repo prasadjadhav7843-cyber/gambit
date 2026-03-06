27af705d211f96348e00e12cb784e6f109c0466c
prasadjadhav7843-cyber <prasadjadhav7843@gmail.com>
2026-03-07 00:34:53 +0530

    ENH: Add __init__.py for games module

---
+++ src/pygambit/games/__init__.py
@@ -0,0 +1,15 @@
+from .bimatrix import (
+    prisoners_dilemma,
+    battle_of_the_sexes,
+    matching_pennies,
+    coordination_game,
+    rock_paper_scissors
+)
+
+__all__ = [
+    "prisoners_dilemma",
+    "battle_of_the_sexes",
+    "matching_pennies",
+    "coordination_game",
+    "rock_paper_scissors"
+]
